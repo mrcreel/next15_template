09c74ea588d4c1735f6737420d9fb6c529bef680
mrcreel <michaelrcreel@gmail.com>
2025-02-05 18:01:08 -0600

    feat(interfaces): add interfaces

--- src/app/components/CenterColumn.tsx
+++ src/app/components/CenterColumn.tsx
@@ -1,6 +1,6 @@
 const CenterColumn = () => {
   return (
-    <section className="flex h-full w-full flex-col bg-yellow-400 py-2">
+    <section className="flex h-full w-full flex-col overflow-y-auto bg-yellow-400 py-2">
       <p>Center Column</p>
     </section>
   )
